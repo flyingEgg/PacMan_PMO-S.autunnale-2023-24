b7519f044dff6199b61f6ec933d6deeb03cf27db
giaco <giaco14mw@gmail.com>
2024-05-16 16:04:33 +0200

    Logica di test per lo stato gameOver()

--- src/DesignPatterns/State/Test.java
+++ src/DesignPatterns/State/Test.java
@@ -15,6 +15,8 @@ import Main.Match;
 import java.util.InputMismatchException;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+import java.util.logging.ConsoleHandler;
 
 public class Test {
 
@@ -46,7 +48,7 @@ public class Test {
                         testGamePause(m, reader);
                         break;
                     case 3:
-                        testGameOver(m);
+                        testGameOver(m, reader);
                         break;
                     case 4:
                         onMenu = false;
@@ -65,48 +67,66 @@ public class Test {
     }
 
     private static void testGameOnGoing(Match p, Scanner pad) {
-        GameState stato = new GameOnGoing(p);
-        String command = pad.next();
-        stato.enterState();
+        GameOnGoing state = new GameOnGoing(p);
+        String command;
+
+        state.enterState();
+        command = pad.next();
         while (true) {
             if (command.equals("p")) {
-                stato.exitState();
+                state.exitState();
                 testGamePause(p, pad);
                 break;
             } else if (command.equals("x")) {
-                stato.exitState();
-                testGameOver(p);
+                state.exitState();
+                testGameOver(p, pad);
                 break;
             }
         }
     }
 
-    private static void testGameOver(Match p) {
-        GameState state = new GameOver(p);
+    private static void testGameOver(Match p, Scanner read) {
+        GameOver state = new GameOver(p);
+        int choice;
 
         // Entrata nello stato di Game Over
         state.enterState();
-        state.exitState();
+        do{
+            System.out.println("1. Si");          // riprova
+            System.out.println("2. Menu (no)");   // vai al menu
+
+            choice = read.nextInt();
+            switch (choice){
+                case 1 -> {
+                    state.exitState();
+                    testGameOnGoing(p, read);
+                }
+                case 2 -> {
+                    state.exitState();
+                    mainMenu(p, read);
+                }
+            }
+        }while (choice != 2);
+
     }
 
     private static void testGamePause(Match p, Scanner read) {
-        GameState stato = new GamePause(p);
+        GamePause state = new GamePause(p);
         int choice;
-        stato.enterState();
+        state.enterState();
         do {
             System.out.println("Menu' di pausa");
             System.out.println("1. Riprendi");
             System.out.println("2. Esci al menu'");
 
             choice = read.nextInt();
-
             switch (choice) {
                 case 1 -> {
-                    stato.exitState();
+                    state.exitState();
                     testGameOnGoing(p, read);
                 }
                 case 2 -> {
-                    stato.exitState();
+                    state.exitState();
                     mainMenu(p, read);
                 }
                 default ->
