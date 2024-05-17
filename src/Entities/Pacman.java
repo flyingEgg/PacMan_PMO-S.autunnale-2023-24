0f0fd9310d5999a3c315d223ed8be544e564c08e
giaco <giaco14mw@gmail.com>
2024-05-17 19:14:57 +0200

    Definizione di costruttore, e metodi della classe Pacman

--- src/Entities/Pacman.java
+++ src/Entities/Pacman.java
@@ -1,4 +1,19 @@
 package Entities;
 
-public class Pacman {
+public class Pacman extends AbstractEntity {
+
+    public Pacman(int x, int y){
+        super(x, y);
+    }
+
+    @Override
+    protected void move(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    @Override
+    protected void draw() {
+
+    }
 }
