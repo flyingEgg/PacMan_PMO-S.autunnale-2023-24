dd253154670075776cd3664dfdead28c9f0822cf
giaco <giaco14mw@gmail.com>
2024-05-19 18:34:58 +0200

    Modifica del type param dell'interfaccia

--- src/API/MovementStrategy.java
+++ src/API/MovementStrategy.java
@@ -3,6 +3,6 @@ package API;
 import Entities.AbstractEntity;
 import Game.Strategies.Direction;
 
-public interface MovementStrategy<P extends AbstractEntity> {
+public interface MovementStrategy<E extends AbstractEntity> {
     void move(Direction direction); // Object al posto di Direction per evitare dipendenze
 }
