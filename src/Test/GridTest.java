3218ddd8326f3b1bea91d678b2eaaeec6f8836be
giaco <giaco14mw@gmail.com>
2024-07-05 00:21:00 +0200

    Creata classe di testing per il Grid

---
+++ src/Test/GridTest.java
@@ -0,0 +1,11 @@
+package Test;
+
+import Entities.Pacman;
+import Game.PacmanGrid;
+
+public class GridTest {
+    public static void main(String[] args){
+        PacmanGrid pacmanGrid = new PacmanGrid();
+        pacmanGrid.printGrid();
+    }
+}
