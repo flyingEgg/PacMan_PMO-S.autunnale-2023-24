ed90f6f21640a9be79f0e95ae2ebeac72650937c
giaco <giaco14mw@gmail.com>
2024-09-06 22:01:47 +0200

    Aggiunto metodo 'handleMagicCoords' all'interfaccia 'MovementStrategy'

--- src/main/java/model/API/MovementStrategy.java
+++ src/main/java/model/API/MovementStrategy.java
@@ -2,6 +2,9 @@ package main.java.model.API;
 
 import main.java.model.Entities.AbstractEntity;
 
+import java.util.Optional;
+
 public interface MovementStrategy<E extends AbstractEntity> {
     void move(Direction direction); // Object al posto di Direction per evitare dipendenze
+    Optional<Position> handleMagicCoords(Position pacPos);
 }
